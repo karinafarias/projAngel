a0b5e7f737ee108d77217d4020200d4ae9c5a7ab
Karina Xavier Farias <karinaxavier@brq.com>
2020-05-31 10:24:24 -0300

    projeto remodelado

--- green/core/views.py
+++ green/core/views.py
@@ -1,19 +1,26 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from core.models import Polo, Terminal
 
 
 # Create your views here.
 def index(request):
-    return redirect('/listaPolo/')
-
-def listaPolos (request):
     polos = Polo.objects.all()
-    dados = {'listaPolo': polos}
-    return render(request, 'index.html',dados)
+    dados = {'listaPolos': polos}
+    return render(request, 'index.html', dados)
+
+def cadastrarPolo(request):
+    return render(request, 'cadastrarPolo.html')
+
+def consultarPolo(request, idPolo):
+    polo = get_object_or_404(Polo, pk=idPolo)
+    dadoPolo = {
+        'polo': polo
+    }
+    terminal = Terminal.objects.filter(idPoloT = polo)
+
+    return render(request, 'consultarPolo.html', dadoPolo)
 
 
-def listaTerminais(request,id):
-    idPoloT = request.GET.get(id)
-    terminais = Terminal.objects.get(idPoloT=id)
-    dados = {'listaTerminal': terminais}
-    return render(request,'consultarPolo.html',dados)
+
+
+
